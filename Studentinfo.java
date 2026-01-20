eb0e1f1df60e2dc5c72119899246a6424a1377a7
oxxximinogyanich <hampniehambert1616@gmail.com>
2026-01-20 13:59:05 +0500

    Create Studentinfo.java

---
+++ Studentinfo.java
@@ -0,0 +1,11 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String fullName = "Данилов Александр Дмитриевич";
+  String group = "ИС 25-21 ТИПО";
+        String program = "Информационные системы";
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+System.out.println("ОП/Специальность: " + program);
+    }
