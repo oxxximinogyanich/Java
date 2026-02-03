68118678c2e49bf19d6ab57e5dc994b461435b8c
eldana <eldana>
2026-02-03 13:51:26 +0500

    da

--- lab1/Main.java
+++ lab1/Main.java
@@ -3,3 +3,16 @@ package lab1;
 public class Main {
 
 }
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        String fullName = "Данилов Александр";
+        String group = "ИС 25-21 ТиПО";
+        String program = "Информационные системы";
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("Специальность: " + program);
+    }
+}
