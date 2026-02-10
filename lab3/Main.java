f69337266768862f0a63f142adb6107386f798ff
oxxximinogyanich <hampniehambert1616@gmail.com>
2026-02-10 13:32:24 +0500

    Add files via upload

---
+++ lab3/Main.java
@@ -0,0 +1,46 @@
+public class Nur {
+
+    public static void main(String[] args) {
+
+        
+        int a = 5;
+        int b = 10;
+        int c = 15;
+
+        System.out.println("Задание 1:");
+        System.out.println("Сумма = " + (a + b + c));
+        System.out.println();
+
+        
+        int x = 20;
+        int y = 4;
+
+        System.out.println("Задание 2:");
+        System.out.println("Сложение: " + (x + y));
+        System.out.println("Умножение: " + (x * y));
+        System.out.println("Деление: " + (x / y));
+        System.out.println();
+
+        
+        int number = 10;
+        number += 5;
+
+        System.out.println("Задание 3:");
+        System.out.println("Значение после присваивания: " + number);
+        System.out.println();
+
+       
+        int n = 6; 
+
+        System.out.println("Задание 4:");
+        System.out.println("Квадрат числа " + n + " = " + (n * n));
+        System.out.println();
+
+        
+        double length = 5.5;
+        double width = 3.2;
+
+        System.out.println("Задание 5:");
+        System.out.println("Площадь прямоугольника = " + (length * width));
+    }
+}
